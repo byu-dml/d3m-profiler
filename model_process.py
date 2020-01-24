5248724c320d94e478ccbd7d737140dc1ad6204d
Joseph Clark <clarkjoe.co@gmail.com>
2020-01-24 13:07:17 -0700

    added RF and confusion matrix

--- model_process.py
+++ model_process.py
@@ -1,7 +1,10 @@
 import logging, csv
 
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import plot_confusion_matrix
 
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
@@ -9,35 +12,41 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 
-
 from build_table import build_table
 from embed import embed
 
 logger = logging.getLogger(__name__)
 
-def svm_process(x_train, x_test, y_train, y_test):
-    svm_clf = svm.SVC()
-    print('Fitting SVM')
-    svm_clf.fit(x_train, y_train)
-    print('Done fitting SVM')
-    print('Predicting colTypes')
-    preds = svm_clf.predict(x_test)
+def run_model(model_class, x_train, x_test, y_train, y_test):
+    model = model_class()
+    # print('Fitting {}'.format(model_class))
+    model.fit(x_train, y_train)
+    # print('Done fitting model')
+    # print('Predicting colTypes')
+    preds = model.predict(x_test)
 
     print(accuracy_score(y_test, preds))
+
+    disp = plot_confusion_matrix(model, x_test, y_test, display_labels=y_test.unique() ,normalize='true')
+
+    print(y_test.unique())
+
+    print(disp.confusion_matrix)
+    plt.show()
+    plt.close()
+
 
 
 if __name__ == '__main__':
     print('Built table complete')
     print('Embedding complete')
-    data = pd.read_csv('train_data_embedded.csv')
+    # data = pd.read_csv('train_data_embedded.csv')
+    data = pd.read_csv('/users/data/d3m/embedded_data_small.csv')
 
     target=data.colType
     features=data.drop('colType', axis=1)
 
     features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.2)
 
-    svm_process(features_train, features_test, target_train, target_test)
-
-
-def rf_processs(x_train, x_test, y_train, y_test):
-    rf_clf = RandomForestClassifier(max_depth=2, random_state=0)
+    run_model(svm.SVC, features_train, features_test, target_train, target_test)
+    run_model(RandomForestClassifier, features_train, features_test, target_train, target_test)
