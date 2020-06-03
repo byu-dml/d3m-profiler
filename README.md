0fa52ca1d388cab4ca1cf5e0489043270a1cd793
Evan Philip Smith <esmith01@smith.aml.cs.byu.edu>
2020-06-03 11:42:18 -0600

    Added simon as a submodule to run our baseline Refactored simon.py Added F1 scores Updated README to include instructions for running baseline

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 To install:
 
 - Clone repo
-- Run the following (preferably in a virualenv): `python -m pip install -e path/to/repo/` 
+- Run the following (preferably in a virtualenv): `python -m pip install -e path/to/repo/` 
 
 Install requirements.txt
 
@@ -21,3 +21,36 @@ Run models with `python3 evaluate_models.py [<number of cores to use>]`
 * Predictions saved to `results[_small]/predictions_<model_name>.csv`
 
 Score predictions with `python3 score_results.py results[_small]/<filename>.csv`
+
+
+
+# Running the baseline with `simon`
+Make sure that the `simon` submodule is initialized. Run this command while inside the `d3m-profiler` directory.
+```
+git submodule update --init --recursive
+```
+Create a new `conda` environment and run the baseline script. See [below](#installing-conda-on-linux-via-command-line) for instructions on how to install `conda`.
+```
+conda create --name simon cudatoolkit=9.0 tensorflow tensorflow-gpu faker keras scikit-learn python-dateutil pandas scipy matplotlib h5py numpy
+conda activate simon
+python simon.py
+```
+**Note**: When creating the `conda` environment, we just specify the version for `cudatoolkit` since `simon` requires CUDA 9.0. `conda` will automatically figure out which version of the other packages to download to maintain compatibility.
+
+# Installing `conda` on Linux via command line
+Navigate to a directory where you want to save the `conda` installer.
+
+Go to the (Anaconda distribution website)[https://www.anaconda.com/products/individual#Downloads] and copy the link for the 64-bit (x86) installer. Download the installer with `curl`.
+
+```
+curl -O https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+```
+**Note** replace the link with the one you copied.
+
+Run the installer script.
+```
+bash Anaconda3-2020.02-Linux-x86_64.sh
+```
+**Note** replace the script name with the version of the one you downloaded.
+
+Follow the prompts. Activate the installation by sourcing your shell's resource file, for example `source ~/.bashrc` or `source ~/.zshrc`.
--- simon.py
+++ simon.py
@@ -3,147 +3,112 @@ import os.path
 import json
 import numpy as np
 import pandas as pd
+from sklearn.metrics import f1_score
+
+import sys
+sys.path.append("./simon")
 from Simon import Simon
 from Simon.Encoder import Encoder
 from Simon.DataGenerator import DataGenerator
 
-from build_table import get_datasets
+from d3m_profiler.build_table import get_datasets
 
+MAX_LEN = 20
+MAX_CELLS = 100
+P_THRESHOLD = 0.3
+CHECKPOINT_DIR = 'simon/Simon/pretrained_models/'
+DATASET_DIR = '/users/data/d3m/datasets'
 
 def main(checkpoint, nb_epoch, batch_size, execution_config):
-    maxlen = 20
-    max_cells = 100
-    p_threshold = 0.3
+    if not os.path.isdir(CHECKPOINT_DIR):
+        os.makedirs(CHECKPOINT_DIR)
 
-    checkpoint_dir = "pretrained_models/"
-    if not os.path.isdir(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
+    raw_data, header, categories = get_raw_data(get_datasets(DATASET_DIR))
+    print(f'Categories: {categories}', flush=True)
 
-    Categories = []
+    start = time.time()
 
-    # get raw_data and header from our data
-    datasets = get_datasets('/users/data/d3m/datasets')
-    # dataset_names, raw_data, header = [], [], []
-    raw_data, header = [], []
+    encoder = Encoder(categories=categories)
+    encoder.process(raw_data, MAX_CELLS)
+    X, y = encoder.encode_data(raw_data, header, MAX_LEN)
+    encoder_max_cells = encoder.cur_max_cells
+    category_count = y.shape[1] # need to know number of fixed categories to create model
+    print('Number of fixed categories is: {}'.format(category_count))
+
+    classifier = Simon(encoder=encoder)
+    data = classifier.setup_test_sets(X, y)
+    model = classifier.generate_model(MAX_LEN, encoder_max_cells, category_count)
+    classifier.load_weights(checkpoint, {}, model, CHECKPOINT_DIR)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
+    history = classifier.train_model(batch_size, CHECKPOINT_DIR, model, nb_epoch, data)
+
+    config = { 'encoder' :  encoder,
+               'checkpoint' : classifier.get_best_checkpoint(CHECKPOINT_DIR) }
+    classifier.save_config(config, CHECKPOINT_DIR)
+    classifier.plot_loss(history) # comment out on docker images...
+
+    evaluate_model(model, data)
+    print(f'Total time taken: {time.time()-start}')
+
+
+def evaluate_model(model, data):
+    start = time.time()
+    scores = model.evaluate(data.X_test, data.y_test, verbose=0)
+    end = time.time()
+    print("Accuracy: %.2f%% \n Time: {0}s \n Time/example : {1}s/ex".format(
+        end - start, (end - start) / data.X_test.shape[0]) % (scores[1] * 100))
+    
+    probabilities = model.predict(data.X_test, verbose=1)
+    prediction_indices = probabilities > P_THRESHOLD
+    y_pred = np.zeros(data.y_test.shape)
+    y_pred[prediction_indices] = 1
+    print(f'F1 micro: {f1_score(data.y_test, y_pred, average="micro")}')
+    print(f'F1 macro: {f1_score(data.y_test, y_pred, average="macro")}')
+    print(f'F1 weighted: {f1_score(data.y_test, y_pred, average="weighted")}')
+
+
+def get_raw_data(datasets):
+    raw_data, header, categories = [], [], []
     count_columns = 0
     for dataset_id, dataset_doc_path in datasets.items():
-
-        # if count_columns > 500: # DEBUG
-        #     break # DEBUG
-
         # open the dataset doc to get the column headers
         columns = None
         with open(dataset_doc_path, 'r') as dataset_doc:
-            # be sure we're skipping w/ the same logic as in build_table.py
             dataset = json.load(dataset_doc)
-            # dataset_name = dataset['about']['datasetName']
-
             for resource in dataset['dataResources']:
-                # if resource['resPath'] != 'tables/learningData.csv':
-                #     print(f'dataset name: {dataset_name}; resource path: {resource["resPath"]}')
-                #     print(f'dataset doc path: {dataset_doc_path}')
-                #     if 'columns' in resource:
-                #         print(f'num columns skipped: {len(resource["columns"])}')
-                #         columns_skipped += len(resource['columns'])
-
-                # if 'resPath' not in resource or resource['resPath'][-4:] != '.csv':
-                #     # print(f'Wrong resPath in a data resource. Dataset ID: {dataset_id}; Resource ID: {resource["resID"]}')
-                #     pass
                 if 'columns' not in resource:
-                    # print(f'No columns found in a data resource. Dataset ID: {dataset_id}; Resource ID: {resource["resID"]}')
-                    pass
-                elif resource['columns'] is not None:
+                    continue
+                # then open the actual dataset table to get column values
+                if resource['resPath'][-4:] == '.csv':
+                    try:
+                        dataset = pd.read_csv(os.path.join(os.path.dirname(dataset_doc_path), resource['resPath']))
+                    except:
+                        continue
+                else:
+                    values = 0
+                    tables = []
+                    for entry in os.scandir(os.path.join(os.path.dirname(dataset_doc_path), resource['resPath'])):
+                        tables.append(pd.read_csv(entry.path))
+                        values += len(tables[-1])
+                        if values >= MAX_CELLS:
+                            break
+                    dataset = pd.concat(tables, ignore_index=True)
 
-                    columns = resource['columns']
-
-                    # then open the actual dataset table to get column values
-                    if resource['resPath'][-4:] == '.csv':
-                        try:
-                            dataset = pd.read_csv(os.path.join(os.path.dirname(dataset_doc_path), resource['resPath']))
-                        except:
-                            continue
+                for column in resource['columns']:
+                    values = list(dataset[column['colName']].values)
+                    if len(values) > MAX_CELLS:
+                        values = [str(v)[:MAX_LEN] for v in values[:MAX_CELLS]]
                     else:
-                        values = 0
-                        tables = []
-                        for entry in os.scandir(os.path.join(os.path.dirname(dataset_doc_path), resource['resPath'])):
-                            tables.append(pd.read_csv(entry.path))
-                            values += len(tables[-1])
-                            if values >= max_cells:
-                                break
-                        dataset = pd.concat(tables, ignore_index=True)
+                        values = [str(v)[:MAX_LEN] for v in values] + ['' for i in range(MAX_CELLS - len(values))]
 
-                    for column in columns:
-                        values = list(dataset[column['colName']].values)
-                        if len(values) > max_cells:
-                            values = [str(v)[:maxlen] for v in values[:max_cells]]
-                        else:
-                            values = [str(v)[:maxlen] for v in values] + ['' for i in range(max_cells - len(values))]
+                    raw_data.append(values)
+                    header.append((column['colType'],))
+                    count_columns += 1
 
-                        # dataset_names.append(dataset_name)
-                        raw_data.append(values)
-                        header.append((column['colType'],))
-                        count_columns += 1
+                    if column['colType'] not in categories:
+                        categories.append(column['colType'])
+    return np.asarray(raw_data), np.asarray(header), categories
 
-                        if column['colType'] not in Categories:
-                            Categories.append(column['colType'])
-
-    del columns
-    del dataset
-
-    print(f'Categories: {Categories}', flush=True)
-
-    # dataset_names = np.asarray(dataset_names)
-    raw_data = np.asarray(raw_data)
-    header = np.asarray(header)
-
-    # return dataset_names, raw_data, header
-
-    start = time.time()
-    print(f'Start time: {start}')
-
-    config = {}
-    encoder = Encoder(categories=Categories)
-    encoder.process(raw_data, max_cells)
-
-    # encode the data
-    X, y = encoder.encode_data(raw_data, header, maxlen)
-
-    max_cells = encoder.cur_max_cells
-
-    Classifier = Simon(encoder=encoder)
-    data = Classifier.setup_test_sets(X, y)
-
-    # print('Sample chars in X:{}'.format(X[2, 0:10]))
-    # print('y:{}'.format(y[2]))
-    # print('a number of items from y:{}'.format(y[:20]))
-
-    # need to know number of fixed categories to create model
-    category_count = y.shape[1]
-    print('Number of fixed categories is: {}'.format(category_count))
-
-    model = Classifier.generate_model(maxlen, max_cells, category_count)
-    
-    Classifier.load_weights(checkpoint, config, model, checkpoint_dir)
-    
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam', metrics=['binary_accuracy'])
-    # start = time.time()
-    history = Classifier.train_model(batch_size, checkpoint_dir, model, nb_epoch, data)
-    # end = time.time()
-    # print("Time for training is %f sec"%(end-start))
-    config = { 'encoder' :  encoder,
-               'checkpoint' : Classifier.get_best_checkpoint(checkpoint_dir) }
-    Classifier.save_config(config, checkpoint_dir)
-    Classifier.plot_loss(history) #comment out on docker images...
-    
-    pred_headers = Classifier.evaluate_model(max_cells, model, data, encoder, p_threshold)
-
-    # pred_headers = Classifier.our_train_and_evaluate(batch_size=batch_size, checkpoint_dir=checkpoint_dir, max_len=maxlen, max_cells=max_cells,
-    #         category_count=category_count, nb_epoch=1, X=X, y=y, encoder=encoder, p_threshold=p_threshold)
-
-    end = time.time()
-    print(f'End time: {end}')
-    print(f'Total time taken: {end-start}')
 
 if __name__ == '__main__':
     import argparse
