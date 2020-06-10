5733706ea9bc3dd6f75e035ccd3bb8c04fc5e899
Benson Manner <bensonmanner13@gmail.com>
2020-06-10 10:50:52 -0600

    multiple models

--- example.py
+++ example.py
@@ -5,16 +5,27 @@ import pandas as pd
 import pickle
 import sys
 from d3m_profiler import rebalance
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier as DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier as RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier as AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB as GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QuadraticDiscriminantAnalysis
+from sklearn.neural_network import MLPClassifier as MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier as KNeighborsClassifier
 from sklearn.model_selection import GroupShuffleSplit
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score, f1_score
 
 
-results = pd.DataFrame(columns=['data_collection', 'classifier', 'balanced', 'accuracy_score', 'f1_score_micro', 'f1_score_macro', 'f1_score_weighted'])
+results = pd.DataFrame(columns=['classifier', 'accuracy_score', 'f1_score_micro', 'f1_score_macro', 'f1_score_weighted'])
 
 type_column = 'colType'
-model = RandomForestClassifier(max_depth=10)
+classifiers = [KNeighborsClassifier,
+    DecisionTreeClassifier,
+    RandomForestClassifier,
+    MLPClassifier,
+    AdaBoostClassifier,
+    GaussianNB]
 
 closed_d3m_file = '../../data_files/data/closed_d3m_data.csv'
 closed_embed = 'embedded_d3m_closed.csv'
@@ -27,38 +38,56 @@ embed_bal_df = pd.read_csv(closed_embed_bal)
 print("Done loading!")
 
 #do shuffled cross validation, but that can also be replicated
-splitter = GroupShuffleSplit(n_splits = 2, train_size=0.66, random_state = 31)
 f1s = list()
 matrices = list()
 print("Beginning cross validation")
-iterator = 1
 X_bal = embed_bal_df.drop(['colType','datasetName'],axis=1)
 y_bal = embed_bal_df['colType']
 dataset_names = embed_bal_df['datasetName']
+splitter = GroupShuffleSplit(n_splits = len(dataset_names.unique()), train_size=0.66, random_state = 31)
 
-#for train_ind, test_ind in splitter.split(embed_bal_df,groups = dataset_names):
-def run_fold(i: int, train_ind: np.ndarray, test_ind: np.ndarray):
-    #now fit on every fold   
-    print("fold_num = "+str(i))
-    model.fit(X_bal.iloc[train_ind],y_bal.iloc[train_ind])
-    y_hat = model.predict(X_bal.iloc[test_ind])
-    y_test = y_bal.iloc[test_ind]
-    f1 = f1_score(y_test, y_hat, labels = y_bal.iloc[train_ind].unique(), average='macro')
-    conf = confusion_matrix(y_test, y_hat, labels=y_bal.iloc[train_ind].unique())
-    return f1, conf
+for i in models:
+    model = i
+    model_name = model.__name__
+    #for train_ind, test_ind in splitter.split(embed_bal_df,groups = dataset_names):
+    def run_fold(i: int, train_ind: np.ndarray, test_ind: np.ndarray):
+        #now fit on every fold   
+        print("fold_num = "+str(i))
+        model.fit(X_bal.iloc[train_ind],y_bal.iloc[train_ind])
+        y_hat = model.predict(X_bal.iloc[test_ind])
+        y_test = y_bal.iloc[test_ind]
+        f1_macro = f1_score(y_test, y_hat, labels = y_bal.iloc[train_ind].unique(), average='macro')
+        f1_micro = f1_score(y_test, y_hat, labels = y_bal.iloc[train_ind].unique(), average='micro')
+        f1_weighted = f1_score(y_test, y_hat, labels = y_bal.iloc[train_ind].unique(), average='weighted')
+        f1_accuracy = accuracy_score(y_test, y_hat)
+        conf = confusion_matrix(y_test, y_hat, labels=y_bal.iloc[train_ind].unique())
+        return f1_macro, f1_micro, f1_weighted, accuracy, conf
 
-def fold_generator(kfold, data, groups):
-    for i, (train_indices, test_indices) in enumerate(kfold.split(data, groups=groups)):
-        yield (i, train_indices, test_indices)    
+    def fold_generator(kfold, data, groups):
+        for i, (train_indices, test_indices) in enumerate(kfold.split(data, groups=groups)):
+            yield (i, train_indices, test_indices)    
     
-mp_pool = mp.Pool()
-results = mp_pool.starmap(run_fold, fold_generator(splitter, embed_bal_df, dataset_names))
-mp_pool.close()    
-mp_pool.join()
-for f1, matrix in results:
-    f1s.append(f1)
-    matrices.append(matrix)
- 
-print(np.mean(f1s))
-print(matrices)    
+    mp_pool = mp.Pool()
+    results_cross = mp_pool.starmap(run_fold, fold_generator(splitter, embed_bal_df, dataset_names))
+    mp_pool.close()    
+    mp_pool.join()
+    f1s_macro = list()
+    f1s_micro = list()
+    f1s_weighted = list()
+    accuracys = list()
+    for f1_macro, f1_micro, f1_weighted, accuracy, matrix in results_cross:
+        f1s_macro.append(f1_macro)
+        f1s_micro.append(f1_micro)
+        f1s_weighted.append(f1_weighted)
+        accuracys.append(accuracy)
+        matrices.append(matrix)
+        
+    mean_f1_macro = np.mean(f1s_macro)    
+    mean_f1_micro = np.mean(f1s_micro)
+    mean_f1_weighted = np.mean(f1s_weighted)
+    mean_accuracy = np.mean(accuracy)     
+        
+    results = results.append({'classifier': model_name, 'accuracy_score': mean_accuracy, 'f1_score_micro': mean_f1_micro, 'f1_score_macro': mean_f1_macro, 'f1_score_weighted': mean_f1_weighted}, ignore_index=True) 
+    
+results.to_csv('final_cross_val.csv',index-False)
 
