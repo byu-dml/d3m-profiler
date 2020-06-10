6449fcdbf33fe49c11666d299564a4e9b6510313
Benson Manner <bensonmanner13@gmail.com>
2020-06-10 09:46:12 -0600

    add multiprocessing capability

--- example.py
+++ example.py
@@ -35,17 +35,25 @@ iterator = 1
 X_bal = embed_bal_df.drop(['colType','datasetName'],axis=1)
 y_bal = embed_bal_df['colType']
 dataset_names = embed_bal_df['datasetName']
-print(X_bal.iloc[[12,34,54]])
-for train_ind, test_ind in splitter.split(embed_bal_df,groups = dataset_names):
+
+#for train_ind, test_ind in splitter.split(embed_bal_df,groups = dataset_names):
+def run_fold(i:int, train_ind: np.ndarray, test_ind: np.ndarray):
     #now fit on every fold   
-    print("n = "+str(iterator))
-    print(X_bal.iloc[train_ind])
+    print("fold_num = "+str(i))
     model.fit(X_bal.iloc[train_ind],y_bal.iloc[train_ind])
     y_hat = model.predict(X_bal.iloc[test_ind])
     y_test = y_bal.iloc[test_ind]
     f1s.append(f1_score(y_test, y_hat, labels = y_bal.iloc[train_ind].unique(), average='macro'))
     matrices.append(confusion_matrix(y_test, y_hat, labels=y_bal.iloc[train_ind].unique()))
-    iterator += 1
+    
+def fold_generator(kfold, data, groups):
+    for i, (train_indices, test_indices) in enumerate(kfold.split(data, groups)):
+        yield (i, train_indices, test_indices, *args)    
+    
+mp_pool = mp.Pool(n_jobs)
+results = mp_pool.starmap(run_fold, fold_generator(splitter, embed_bal_df, dataset_names))
+mp_pool.close()    
+
 print(np.mean(f1s))
 print(matrices)    
 
