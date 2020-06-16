2f97adbb7d203741871205f83697db6568b6f87e
Eric Manner <ebensonm@login01.rc.byu.edu>
2020-06-16 11:26:47 -0600

    changes

--- example.sh
+++ example.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --time=12:00:00   # walltime
-#SBATCH --ntasks=16   # number of processor cores (i.e. tasks)
-#SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=12288M   # memory per CPU core
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --ntasks=50   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=10   # number of nodes
+#SBATCH --mem-per-cpu=1024M   # memory per CPU core
 #SBATCH -J "Profiler Tester"   # job name
 
 #add the right thing to the file path
@@ -13,4 +13,4 @@ export PATH="$HOME/.local/bin:$PATH"
 module load python/3.8
 
 #now run the script
-python -u example.py
+srun python -u example.py
