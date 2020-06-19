da687b70ed0f31633027f4be819544a98471d680
Eric Manner <ebensonm@2yam.aml.cs.byu.edu>
2020-06-19 17:39:11 -0600

    embedding function updates

--- save_embed.py
+++ save_embed.py
@@ -21,38 +21,38 @@ def embed(df: pd.DataFrame, model_weights_path: str):
 
     dataset_names = df['datasetName']
     print("Starting Embedding")
-    dataset_name_embs = tqdm(model.encode(df['datasetName'].str.lower().to_numpy()))
+    dataset_name_embs = model.encode(df['datasetName'].to_numpy())
     print("done_1!")
-    description_embs = model.encode(df['description'].str.lower().to_numpy())
+    description_embs = model.encode(df['description'].to_numpy())
     print("done_2!")
-    col_name_embs = model.encode(df['colName'].str.lower().to_numpy())
-    print(np.shape(col_name_embs))
-    print(np.shape(description_embs))
-    col_types = df[type_column]
+    col_name_embs = model.encode(df['colName'].to_numpy())
+    #print(np.shape(col_name_embs))
+    #print(np.shape(description_embs))
+    col_types = df['colType']
 
     group_type_df = pd.DataFrame({'datasetName': dataset_names, 'colType': col_types})
-    embeddings_df = pd.DataFrame(data=np.hstack((dataset_name_embs, description_embs, col_name_embs)), columns=['emb_{}'.format(i) for i in range(len(col_name_embs[0]))])
+    embeddings_df = pd.DataFrame(data=np.hstack((dataset_name_embs, description_embs, col_name_embs)), columns=['emb_{}'.format(i) for i in range(3*len(col_name_embs[0]))])
 
-    return pd.concat([group_type_df, embeddings_df], axis=1)
+    return pd.concat([group_type_df.reset_index(), embeddings_df.reset_index()], axis=1).drop(['index'],axis=1)
+
 if __name__=="__main__":
     #load the original csv
     print("loading...")
-    data_df = pd.read_csv(open_d3m_file)
+    data_df = pd.read_csv(closed_d3m_file).applymap(str)
+    #data_df = data_df.sample(100)
     #split the dataFrame into equal parts
     print("splitting...")
     splits = np.array_split(data_df, _NUM_THREAD)
-    processes = []
+    list_in = list()
+    for i in range(len(splits)):
+        list_in.append((splits[i],model_weights_path))
     print("starting multiprocess")
-    for i in range(0,_NUM_THREAD):
-        p = mp.Process(target=embed,args=(splits[i],model_weights_path,))
-        processes.append(p)
-        p.start()
-        
-    for p in processes:
-        p.join()
-        
-    print(p)
-    #df = pd.concat(p)
+    mp_pool = mp.Pool()
+    results = mp_pool.starmap(embed, list_in)
+    mp_pool.close()
+    mp_pool.join()
+    
+    df = pd.concat(results,axis=0).reset_index().drop(['index'],axis=1)
     #save the results
-    #df.to_csv('closed_embed_all.csv',index=False)
+    df.to_csv('closed_embed_all.csv',index=False)
     
