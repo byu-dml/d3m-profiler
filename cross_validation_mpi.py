3dbceced1f1eff87658184d0043b5c3600c5d8bf
Benson Manner <bensonmanner13@gmail.com>
2020-06-19 14:24:49 -0600

    cross validation updates

--- cross_validation_mpi.py
+++ cross_validation_mpi.py
@@ -3,31 +3,97 @@ import numpy as np
 import os
 import pathlib as pl
 import pandas as pd
+from d3m_profiler.build_table import get_datasets
+import json
+import time
 import pickle
 import sys
 from mpi4py import MPI
 from imblearn.over_sampling import SMOTE
-#from d3m_profiler import rebalance
-#from sklearn.tree import DecisionTreeClassifier as DecisionTreeClassifier
+from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.ensemble import RandomForestClassifier as RandomForestClassifier
+#from sklearn.neural_network import MLPClassifier as MLPClassifier
 #from sklearn.ensemble import AdaBoostClassifier as AdaBoostClassifier
 #from sklearn.naive_bayes import GaussianNB as GaussianNB
 #from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QuadraticDiscriminantAnalysis
 #from sklearn.neighbors import KNeighborsClassifier as KNeighborsClassifier
 #from sklearn.pipeline import Pipeline
 #from sklearn.decomposition import PCA
+#from sklearn.tree import DecisionTreeClassifier as DecisionTreeClassifier
 #from sklearn.neural_network import MLPClassifier as MLPClassifier
-from sklearn.model_selection import GroupShuffleSplit, LeaveOneGroupOut
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score, f1_score
 
-def evaluate_model(rank,balance,col_type):
+DATASET_DIR = '/users/data/d3m/datasets/training_datasets'
+METADATA_PATH = '~/data/closed_d3m_unembed_data.csv'
+embed_col_path = ''
+embed_all_path = ''
+MAX_LEN = 20
+MAX_CELLS = 100
+
+def parse_datasets(datasets):
+    raw_data, header, groups = [], [], []
+    for dataset_id, dataset_doc_path in datasets.items():
+        # open the dataset doc to get the column headers
+        with open(dataset_doc_path, 'r') as dataset_doc:
+            meta_dataset = json.load(dataset_doc)
+            for resource in meta_dataset['dataResources']:
+                if 'columns' not in resource:
+                    continue
+                # then open the actual dataset table to get column values
+                if resource['resPath'][-4:] == '.csv':
+                    try:
+                        dataset = pd.read_csv(os.path.join(os.path.dirname(dataset_doc_path), resource['resPath']))
+                    except:
+                        continue
+                else:
+                    values = 0
+                    tables = []
+                    for entry in os.scandir(os.path.join(os.path.dirname(dataset_doc_path), resource['resPath'])):
+                        tables.append(pd.read_csv(entry.path))
+                        values += len(tables[-1])
+                        if values >= MAX_CELLS:
+                            break
+                    dataset = pd.concat(tables, ignore_index=True)
+
+                for column in resource['columns']:
+                    values = list(dataset[column['colName']].values)
+                    if len(values) > MAX_CELLS:
+                        values = [str(v)[:MAX_LEN] for v in values[:MAX_CELLS]]
+                    else:
+                        values = [str(v)[:MAX_LEN] for v in values] + ['' for i in range(MAX_CELLS - len(values))]
+
+                    raw_data.append(values)
+                    header.append((column['colType'],))
+                    groups.append(meta_dataset['about']['datasetName'])
+                    
+    return np.asarray(raw_data), np.asarray(header), np.asarray(groups)
+  
+def save_results(results: pd.dataFrame):
+    #save the results to a csv file
+    results.to_csv(model_name+'_final_cross_val.csv',index=False)
+    filename = model_name+'_matrix_mean.pkl'
+    fileObject = open(filename, 'wb')
+    pickle.dump(conf_mean, fileObject)
+    fileObject.close()
+    
+def naive_gen():
+    model_name = 'Naive'
+    class NaiveModel:
+        def fit(self,X_train,y_train):
+            self.majority = y_train.value_counts().idxmax()
+        def predict(self,X_test):
+            y_hat = [self.majority for i in range(len(X_test))]
+            return y_hat
+    model = NaiveModel()
+    return model
+
+def evaluate_model(balance: bool, col_name: bool, use_metadata: bool, rank=None):
 
     def run_fold(train_ind, test_ind, balance=True):
         #now fit using the indeces given by the kfold splitter
         X_train = X_data[train_ind]
         y_train = y.iloc[train_ind]
-        #labels = y_train.unique()
         #get the labels for the confusion matrix
         if (balance == True):
             #get the k_neighbors balance number
@@ -50,21 +116,32 @@ def evaluate_model(rank,balance,col_type):
   
     if (rank == 0):
         print("Beginning cross validation")
-        type_column = 'colType'
-        closed_embed = 'embedded_d3m_closed.csv'
-    
-        print("loading file")
-        embed_df = pd.read_csv(closed_embed)
-        print("Done loading!")
-    
-        #do shuffled cross validation, but that can also be replicated
-        X_data = embed_df.drop(['colType','datasetName'],axis=1).to_numpy()
-        y = embed_df['colType']
-        dataset_names = embed_df['datasetName']
+        if (use_metadata):
+            if (col_name is True):
+                type_column = 'colType'
+                closed_embed = 'embedded_d3m_closed.csv'    
+                print("loading file")
+                data = pd.read_csv(closed_embed)
+                print("Done loading!")
+                #do shuffled cross validation, but that can also be replicated
+                X_data = data.drop(['colType','datasetName'],axis=1).to_numpy()
+                y = data['colType']
+                groups = data['datasetName']
+            else:
+                type_column = 'colType'
+                closed_embed = 'embedded_d3m_closed_all.csv'
+                print("loading file")
+                data = pd.read_csv(closed_embed)
+                print("Done loading!")
+                X_data = data.drop(['colType','datasestName'],axis=1).to_numpy()
+                y = data['colType']
+                groups = data['datasetName']
+        else:
+            X_data, y, groups = parse_dataset(get_datasets(DATASET_DIR))
+            
         k_splitter = LeaveOneGroupOut()
         split_num = k_splitter.get_n_splits(embed_df, groups=dataset_names)
         jobs = list(k_splitter.split(embed_df, groups=dataset_names))
-    
         #gets the jobs and splits them into even-sized-lists to be spread across the different cpu's
         list_jobs_total = [list() for i in range(COMM.size)]
         for i in range(len(jobs)):
@@ -72,11 +149,17 @@ def evaluate_model(rank,balance,col_type):
             list_jobs_total[j].append(jobs[i])
         jobs = list_jobs_total
     else:
-        #initalizes variables to pass to other processors, size of X_data is intialized correctly
-        X_data = np.empty((47831, 768),dtype='d')
+        #initalizes variables to pass to other processors, size of X_data must be intialized correctly
+        if (use_metadata):
+            if (col_name is True):
+                X_data = np.empty((47831, 768),dtype='d')
+            else:
+                X_data = np.empty((487831, 768*3), dtype='d')
+        else:
+                X_data = np.empty((shape_data), dtype='d')  
         y = None
         jobs = None
-
+           
     #get the values from the root processor
     y = COMM.bcast(y,root=0)
     jobs = COMM.scatter(jobs, root=0)
@@ -98,48 +181,31 @@ def evaluate_model(rank,balance,col_type):
         jobs = list()
         X_data = list()
         y = list()
-        results = pd.DataFrame(columns=['classifier', 'accuracy_score', 'f1_score_micro', 'f1_score_macro', 'f1_score_weighted'])
         print("Finished cross validation!")
         #flatten the total results
         results_final = [_i for temp in results_init for _i in temp]
         y_test = list()
         y_hat = list()
-        #compute the means of the results
+        #compute the results
         for hat, test in results_final:
             y_test += test
             y_hat += hat
-        mean_accuracy = accuracy_score(y_test, y_hat)
-        mean_f1_macro = f1_score(y_test, y_hat, average='macro')
-        mean_f1_micro = f1_score(y_test, y_hat, average='micro')
-        mean_f1_weighted = f1_score(y_test, y_hat, average='weighted')
-        conf_mean = confusion_matrix(y_test, y_hat) 
-        results = results.append({'classifier': model_name, 'accuracy_score': mean_accuracy, 'f1_score_micro': mean_f1_micro, 'f1_score_macro': mean_f1_macro, 'f1_score_weighted': mean_f1_weighted}, ignore_index=True) 
-        #save the results to a csv file
-        results.to_csv(model_name+'_final_cross_val.csv',index=False)
-        #conf_mean = np.sum(confusions,axis=0) / len(confusions)
-        filename = model_name+'_matrix_mean.pkl'
-        fileObject = open(filename, 'wb')
-        pickle.dump(conf_mean, fileObject)
-        fileObject.close()
+        accuracy = accuracy_score(y_test, y_hat)
+        f1_macro = f1_score(y_test, y_hat, average='macro')
+        f1_micro = f1_score(y_test, y_hat, average='micro')
+        f1_weighted = f1_score(y_test, y_hat, average='weighted')
+        conf = confusion_matrix(y_test, y_hat) 
+        results = results.append(data=[model_name, mean,f1_macro, f1_micro, f1_weighted], columns=['classifier', 'accuracy_score', 'f1_score_micro', 'f1_score_macro', 'f1_score_weighted'])
+        save_results(results) 
         return results
     
 if __name__ == "__main__":
-    #define the model
-    #model_name = 'Naive'
-    #class NaiveModel:
-    #    def fit(self,X_train,y_train):
-    #        self.majority = y_train.value_counts().idxmax()
-    #    def predict(self,X_test):
-    #        y_hat = [self.majority for i in range(len(X_test))]
-    #        return y_hat
-    #model = NaiveModel()
     random_state = 32
     model_name = 'RF'
-    model = RandomForestClassifier(max_depth=10,random_state=random_state)
-    
+    model = RandomForestClassifier(max_depth=10,random_state=random_state) 
     COMM = MPI.COMM_WORLD
     rank = COMM.rank
-    results = evaluate_model(rank,balance=True,col_type=True)
+    results = evaluate_model(balance=True,col_name=True,use_metadata=True, rank=rank)
     
 
 
