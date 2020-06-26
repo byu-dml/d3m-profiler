7c74cc43b6cb43ddaa1908957d77dbbdccef87dc
Evan Smith <evanphilipsmith@gmail.com>
2020-06-25 19:22:15 -0700

    Clean up imports

--- example.py
+++ example.py
@@ -1,17 +1,13 @@
 import numpy as np
 import multiprocessing as mp
-import pathlib as pl
 import pandas as pd
 import pickle
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC as SupportVectorClassifier
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC as SupportVectorClassifier
 
-from d3m_profiler import rebalance, score_results
-from d3m_profiler.evaluate_models import run_models, _save_results
+from d3m_profiler import rebalance
 from d3m_profiler.embed import embed
 
 _NUM_THREADS = mp.cpu_count()
