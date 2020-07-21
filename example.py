52ed5e8ca39dc522a89262360c708d0731e93798
Benson Manner <bensonmanner13@gmail.com>
2020-07-21 17:49:26 -0600

    added mlp model

--- example.py
+++ example.py
@@ -20,13 +20,18 @@ simon_embed_path = 'Simon_embed.pkl'
 file_to_save = 'rf_v_simon.csv'
 
 model_comp = RandomForestClassifier(random_state=30)
+model_comp_1 = MLPClassifier(random_state=21)
 #define the split_type
-split_type = ShuffleSplit(n_splits=1, train_size=0.7, random_state=21)
+split_type = GroupShuffleSplit(n_splits=9, test_size=0.3, random_state=31)
 #create the models
-model_SIMON = BaselineSimon(split_type=split_type, embed_data_file=simon_embed_path, data_path=simon_path, model_name='Simon_Shuffle_1')
-model_random_forest = MetaDataProfiler(model=model_comp, use_col_name_only=True, embedding_type='SentenceTransformer', EMBEDDING_WEIGHTS_PATH=weights_path, model_name='RandomForest_ColName_Shuffle_1', balance_type='SMOTE', balance='True', embed_data_file=meta_embed_path, split_type=split_type, data_path=meta_data_path)
+#simon model
+model_SIMON = BaselineSimon(split_type=split_type, embed_data_file=simon_embed_path, data_path=simon_path, model_name='Simon_GroupShuffle_9')
+#random forest model
+model_random_forest = MetaDataProfiler(model=model_comp, use_col_name_only=True, embedding_type='SentenceTransformer', EMBEDDING_WEIGHTS_PATH=weights_path, model_name='RandomForest_ColName_GroupShuffle_9', balance_type='SMOTE', balance='True', embed_data_file=meta_embed_path, split_type=split_type, data_path=meta_data_path)
+#mlp classifier
+model_mlp = MetaDataProfiler(model=model_comp_1, use_col_name_only=True, embedding_type='SentenceTransformer', EMBEDDING_WEIGHTS_PATH=weights_path, model_name='MLP_ColName_GroupShuffle_9', balance_type='SMOTE', balance='True', embed_data_file=meta_embed_path, split_type=split_type, data_path=meta_data_path)
 
 #get both the models to run with the profiler
-initialized_models = [model_SIMON, model_random_forest]
+initialized_models = [model_SIMON, model_random_forest, model_mlp]
 #calling this will save all of the initialized model score results to the save_results_file
 evaluate_models.run_models(initialized_models=initialized_models, save_results_file=file_to_save)
