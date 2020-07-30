be96e75be842c7db69cb15e8a3e87dfe2bfd52d6
Evan Smith <evanphilipsmith@gmail.com>
2020-07-30 13:03:42 -0600

    put model weights directory as an environment variable for convenience

--- MetadataProfiler.py
+++ MetadataProfiler.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from sklearn.ensemble import RandomForestClassifier
 from ModelBase import ModelBase
 from d3m_profiler.embed import embed
@@ -8,7 +9,7 @@ from d3m_profiler.rebalance import rebalance_SMOTE as rebalance
 class MetadataProfiler(ModelBase):
     def __init__(self, model=RandomForestClassifier):
         super().__init__()
-        self.MODEL_WEIGHTS_PATH = 'torontobooks_unigrams.bin'
+        self.model_weights_path = os.getenv('MODEL_WEIGHTS_DIR', '') + 'torontobooks_unigrams.bin'
         self.model = model()
 
     def fit(self, X, y):
@@ -23,7 +24,7 @@ class MetadataProfiler(ModelBase):
         self.model.fit(X, y)
 
     def encode_data(self, X, y):
-        return embed(X, self.MODEL_WEIGHTS_PATH), y
+        return embed(X, self.model_weights_path), y
 
     def predict(self, X):
         return self.model.predict(X)
